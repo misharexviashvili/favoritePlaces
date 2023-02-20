5b71a77051760eadb489ab5479e515fe4bedd806
Misha <misharexviashvili@yahoo.com>
2023-02-21 00:34:37 +0400

    branching

--- components/PlacesList.js
+++ components/PlacesList.js
@@ -1,5 +1,5 @@
 import { FlatList } from "react-native";
 
 export default function PlacesList({ places }) {
-  return <FlatList data={places} keyExtractor={(item) => item.id} renderItem={()=>}/>;
+  return <FlatList data={places} keyExtractor={(item) => item.id} /*renderItem*/ />;
 }
