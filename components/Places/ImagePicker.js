9f1d24b296806972a85ec0a7a67fc208c2fd2170
Misha <misharexviashvili@yahoo.com>
2023-02-22 00:44:50 +0400

    added taking photo function

---
+++ components/Places/ImagePicker.js
@@ -0,0 +1,19 @@
+import { View, Button } from "react-native";
+import { launchCameraAsync } from "expo-image-picker";
+
+export default function ImagePicker() {
+  async function takeImageHandler() {
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    console.log(image);
+  }
+  return (
+    <View>
+      <View></View>
+      <Button title="Take Image" onPress={takeImageHandler} />
+    </View>
+  );
+}
--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 import { Colors } from "../../constants/colors";
+import ImagePicker from "./ImagePicker";
 
 export default function PlaceForm() {
   const [enteredTitle, setEnteredTitle] = useState("");
@@ -16,6 +17,7 @@ export default function PlaceForm() {
           onChangeText={changeTitleHandler}
           value={enteredTitle}
         />
+        <ImagePicker/>
       </View>
     </ScrollView>
   );
