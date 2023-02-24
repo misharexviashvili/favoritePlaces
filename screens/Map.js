65bc566c580dd830d2cbc0875863ef70d7bde14f
Misha <misharexviashvili@yahoo.com>
2023-02-25 00:08:01 +0400

    pock on map button now opens up map, Tbilisi by default

--- screens/Map.js
+++ screens/Map.js
@@ -1,9 +1,19 @@
-import { View, Text } from "react-native";
+import { StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function Map() {
-  return (
-    <View>
-      <Text>Map</Text>
-    </View>
-  );
+  // Object like this must be passed to initialRegion
+  const region = {
+    latitude: 41.7151,
+    longitude: 44.8271,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+  // MapView supports style prop, it needs to be filled, otherwise map will not be visible
+  return <MapView style={styles.map} initialRegion={region}></MapView>;
 }
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+});
