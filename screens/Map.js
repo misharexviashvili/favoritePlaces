d5a3cda437ba0920d3348a224abc6868a6105818
Misha <misharexviashvili@yahoo.com>
2023-02-25 00:48:22 +0400

    place with marker can be saved now

--- screens/Map.js
+++ screens/Map.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
-import { StyleSheet } from "react-native";
+import { useCallback, useLayoutEffect, useState } from "react";
+import { Alert, StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import IconButton from "../components/UI/IconButton";
 
-export default function Map() {
+export default function Map({ navigation }) {
   const [selectedLocation, setSelectedLocation] = useState();
   // Object like this must be passed to initialRegion
   const region = {
@@ -20,6 +21,29 @@ export default function Map() {
     //  Basically the same as {lat: lat, lng: lng}
     setSelectedLocation({ lat, lng });
   }
+  const savePickedLocationHandler = useCallback(() => {
+    if (!selectedLocation) {
+      Alert.alert("No location picked", "Pick a location by tapping on a map");
+      return;
+    }
+    // This passes data back from map which was open and tapped to AddPlace component (where we choose image, location and so on)
+    navigation.navigate("AddPlace", {
+      pickedLat: selectedLocation.lat,
+      pickedLng: selectedLocation.lng,
+    });
+  }, [navigation, selectedLocation]);
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ({ tintColor }) => (
+        <IconButton
+          icon="save"
+          size={24}
+          color={tintColor}
+          onPress={savePickedLocationHandler}
+        />
+      ),
+    });
+  }, [navigation, savePickedLocationHandler]);
   // MapView supports style prop, it needs to be filled, otherwise map will not be visible
   return (
     <MapView
